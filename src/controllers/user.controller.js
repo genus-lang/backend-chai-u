3a33048e158eb3633c0e69d7d8b8075e15f75088
genus-lang <rinku4578914@gmail.com>
2026-01-29 18:34:39 +0530

    add register controller and bug fixed

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,9 +1,15 @@
 import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler (async (req, res) => {
-    res.status(201).json({
-        message: "Postman connected successfully to register user endpoint",
-    });
+    //get user details from the frontend
+    //validation  -not empty
+    //checking if user already exists
+    //checking for the images , check for avatar
+    // upload them to cloudinary
+    //create user object - create entery in db
+    //remove password and refresh tocken field from the response
+    //check for user creation
+    //return response to frontend
 });
 
 
