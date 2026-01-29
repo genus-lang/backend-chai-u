9d75d11b7210f0acd491a31260ce788aed42d4e6
genus-lang <rinku4578914@gmail.com>
2026-01-29 14:29:12 +0530

    user controller updated

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -2,7 +2,7 @@ import {asyncHandler} from "../utils/asyncHandler.js";
 
 const registerUser = asyncHandler (async (req, res) => {
     res.status(201).json({
-        message: "ok",
+        message: "Postman connected successfully to register user endpoint",
     });
 });
 
